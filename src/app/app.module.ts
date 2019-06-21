f42c51a7884583c3d66eb1bc4f82b317d363504d
Matt <matthew.john.bischoff@gmail.com>
2019-06-21 10:35:25 -0400

    removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {HashLocationStrategy, Location, LocationStrategy} from '@angular/common';
+import { HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
 import { TopicsListComponent } from './topics-list/topics-list.component';
 import { TopicDetailsComponent } from './topic-details/topic-details.component';
 import { ChartsModule } from 'ng2-charts';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login.service';
 import { VotesService } from './votes.service';
-
 
 
 @NgModule({
@@ -30,28 +24,24 @@ import { VotesService } from './votes.service';
     ChartsModule,
     RouterModule.forRoot([
       { path: '', component: TopicsListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent},
-      { path: 'cart', component: CartComponent},
-      { path: 'login', component: LoginComponent},
-      { path: 'shipping', component: ShippingComponent },
+      { path: 'login', component: LoginComponent },
       { path: 'topics', component: TopicsListComponent },
-      { path: 'products', component: ProductListComponent },
       { path: 'topics/:topicID', component: TopicDetailsComponent },
-  ])
+    ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    ShippingComponent,
     TopicsListComponent,
     TopicDetailsComponent,
     LoginComponent
   ],
-  providers: [Location, {provide: LocationStrategy, useClass: HashLocationStrategy},LoginService, VotesService],
-  bootstrap: [ AppComponent ]
+  providers: [
+    Location,
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    LoginService,
+    VotesService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
